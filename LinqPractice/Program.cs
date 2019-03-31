65b3fcfea510910b8971213bf3749cb5579807c1
McDonnes <stephen.mcdonnell.44@gmail.com>
2019-03-31 13:57:15 -0500

    Restructured into static methods of main rather than all problems solved in main and reformatted the output to display a little cleaner

--- LinqPractice/Program.cs
+++ LinqPractice/Program.cs
@@ -10,41 +10,46 @@ namespace LinqPractice
     {
         static void Main(string[] args)
         {
-            //Linq Problem 1
-            var words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics"};
+            Console.WriteLine("Problem 1: ");
+            ProblemOne(new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" });
+            Console.WriteLine("\n\nProblem 2: ");
+            ProblemTwo(new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" });
+            Console.WriteLine("\n\nProblem 3: ");
+            ProblemThree(new List<string>(){
+                    "80,100,92,89,65",
+                    "93,81,78,84,69",
+                    "73,88,83,99,64",
+                    "98,100,66,74,55"
+            });
+            Console.WriteLine("\n\nProblem 4: ");
+            ProblemFour("Terril");
+            Console.ReadLine();
+        }
+        static void ProblemOne(List<string> words)
+        {
             var thWords = words.Where(w => w.Contains("th"));
-
-            foreach(var result in thWords)
+            foreach (var result in thWords)
             {
                 Console.WriteLine(result);
             }
-            Console.ReadLine();
-
-            //Linq Problem 2
-            var names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+        }
+        static void ProblemTwo(List<string> names)
+        {
             var noDuplicateNames = names.Distinct();
 
             foreach (var result in noDuplicateNames)
             {
                 Console.WriteLine(result);
             }
-            Console.ReadLine();
-
-            //Linq Problem 3
-            var grades = new List<string>(){
-                    "80,100,92,89,65",
-                    "93,81,78,84,69",
-                    "73,88,83,99,64",
-                    "98,100,66,74,55"
-            };
+        }
+        static void ProblemThree(List<string> grades)
+        {
             var intGrades = grades.Select(g => (Array.ConvertAll(g.Split(','), int.Parse)));
             var AvgNoMinGrades = intGrades.Select(g => g.Where(x => g.Min() != x).Average()).Average();
             Console.WriteLine(AvgNoMinGrades);
-            Console.ReadLine();
-
-
-            //Linq Problem 4
-            String word = "Spinney";
+        }
+        static void ProblemFour(string word)
+        {
             var condense = (word.ToUpper().ToCharArray()).GroupBy(p => p).ToDictionary(g => g.Key, g => g.Count()).OrderBy(g => g.Key);
             foreach (KeyValuePair<char, int> x in condense)
             {
@@ -53,8 +58,6 @@ namespace LinqPractice
                 int temp2 = x.Value;
                 Console.Write(temp2);
             }
-            Console.ReadLine();
-
         }
     }
 }
